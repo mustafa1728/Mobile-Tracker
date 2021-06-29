7fb8c9bf487a658aa2362b50780cff9a2f9252ce
Mustafa Chasmai <mustchasmai@gmail.com>
2021-06-29 23:25:01 +0530

    add main.py code stub

---
+++ main.py
@@ -0,0 +1,11 @@
+from network.routing import RoutingMapTree
+
+def main():
+	database = RoutingMapTree()
+	# insert some exchanges with heirarchy
+	# insert some mobiles
+	# run simulation
+
+
+if __name__ == "__main__":
+    main()
